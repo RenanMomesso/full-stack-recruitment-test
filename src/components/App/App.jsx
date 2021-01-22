6c1b3d0907b263a9fabbad5bac0ccbf984ee4ee2
Renan Momesso <Renan>
2021-01-22 14:20:16 -0300

    teste_renan

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import Routes from '../../routes';
+import {createGlobalStyle} from 'styled-components'
+
+const GlobalStyle = createGlobalStyle `
+  body {
+  margin: 0;
+  font-family:sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-color: cadetblue;
+  }
+`;
 
 const App = () => (
-  <main>
-    <h1>Over to you...</h1>
-  </main>
+  <>
+  <GlobalStyle/>
+  <Routes/>
+
+  </>
 );
 
 export default App;
